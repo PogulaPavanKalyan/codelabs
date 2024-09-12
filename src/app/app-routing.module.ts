bf217d62a2d71efc1cacb1e49cad46043e4b11d8
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-09-12 22:58:08 +0530

    added the first changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'courses',
+  component: CoursesComponent
+}
+  , {
+  path: 'contact-us',
+  component: ContactUsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
