f9665be6c5935aa465155937070c23e06c480b70
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-09-27 00:48:20 +0530

    added the course page and some content

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currentRouteName: string = '';
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  }
 
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Get the current route after navigation ends
+      const route = this.getChild(this.activatedRoute);
+      route.data.subscribe(data => {
+        this.currentRouteName = data['title'] || this.router.url;
+        console.log('Current Route Name:', this.currentRouteName);
+      });
+    });
+  }
+  getChild(activatedRoute: ActivatedRoute): ActivatedRoute {
+    if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
+    } else {
+      return activatedRoute;
+    }
+  }
 }
