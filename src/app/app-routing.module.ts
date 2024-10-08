cb58808815ffec9cddbd228646081ca649e91d33
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-10-08 23:37:09 +0530

    ksnk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Components/home/home.component';
-import { CoursesComponent } from './Components/courses/courses.component';
-import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { HomeComponent } from './Components(copy)/home/home.component';
+import { CoursesComponent } from './Components(copy)/courses/courses.component';
+import { ContactUsComponent } from './Components(copy)/contact-us/contact-us.component';
 
 const routes: Routes = [{
   path: '',
