c024895ce8b8933c9612be72952434bdd33eb93f
vivkananda Reddy <vivekanandapolu123@gmail.com>
2024-10-09 08:13:45 +0530

    added changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './Components(copy)/courses/courses.component';
+import { CoursesComponent } from './Components/courses/courses.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { ContactUsComponent } from './Components(copy)/contact-us/contact-us.component';
-import { HomeComponent } from './Components(copy)/home/home.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
